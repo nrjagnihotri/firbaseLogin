7567a963f7730e6325a23c67a2ee38dcb4169742
reacttut2 <98811632+nrjagnihotri@users.noreply.github.com>
2022-09-06 11:44:24 +0530

    Add files via upload

---
+++ firebase/index.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/database";
+import "firebase/compat/auth";
+
+var firebaseConfig = {
+    // your firebase configuration goes here
+};
+
+firebase.initializeApp(firebaseConfig);
+
+//const db = firebaseDB.database().ref();
+const auth = firebase.auth();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+const facebookAuthProvider = new firebase.auth.FacebookAuthProvider();
+
+export { auth, googleAuthProvider, facebookAuthProvider };
+
